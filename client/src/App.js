a433b673960cf095ed6ad60a3ca8f7672e089cba
marl-Am <marl-Am@users.noreply.github.com>
2023-06-19 20:34:15 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ export default function App() {
       <div className='h'>
         <>
           <button
-            className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3  shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            className="mb-5 bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3  shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             type="button"
             onClick={() => setShowModal(true)}
           >
