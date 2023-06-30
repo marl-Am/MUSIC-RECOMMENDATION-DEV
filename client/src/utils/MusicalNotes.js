7ffcb4ae0937827baefe5e9bbe14851d7fa7bc79
marl-Am <marl-Am@users.noreply.github.com>
2023-06-29 23:40:35 -0400

    Create MusicalNotes.js

---
+++ client/src/utils/MusicalNotes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './MusicalNotes.css';
+
+function MusicalNotes() {
+    return (
+        <div className='musical-notes-container'>
+            <div className="muzieknootjes">
+                <div className="noot-1">
+                    &#9835; &#9833;
+                </div>
+                <div className="noot-2">
+                    &#9833;
+                </div>
+                <div className="noot-3">
+                    &#9839; &#9834;
+                </div>
+                <div className="noot-4">
+                    &#9834;
+                </div>
+            </div>
+        </div>
+    );
+}
+export default MusicalNotes;
